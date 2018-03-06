9227a55fa2c1a3671bbfa90e8aa16e9924d19b7d
Adarsh Lilha <adarshlilha@gmail.com>
2018-03-06 19:42:47 +0530

    Refactor code

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Rotten Tomatoes Critics vs Audience
 ### Top movies liked by critics not by audience
 
-> A scatter plot chart showing movies liked by critics but not by audience, from 2007 to 2017. 
+A bubble plot chart showing movies liked by critics but not by audience, from 2007 to 2017. 
 
-> Uses [d3js](https://d3js.org/) for chart generation and simulated annealing for removal of label overlaps
 
-> With reference from annealing implementation of [d3-labeler](https://github.com/tinker10/D3-Labeler)
+Uses [d3js](https://d3js.org/) for chart generation and [Simulated Annealing](https://en.wikipedia.org/wiki/Simulated_annealing) for removal of label overlaps
+
+
+With reference from annealing implementation of [d3-labeler](https://github.com/tinker10/D3-Labeler)
 
 #### Check out the hosted version at [https://adarshlilha.github.io/movie-gap-rt/](https://adarshlilha.github.io/movie-gap-rt/)
 
@@ -13,4 +15,6 @@
 - `git clone` - clone or download this repository
 - serve the folder with a local server
 
->python -m SimpleHTTPServer 8000
+``` 
+python -m SimpleHTTPServer 8000 
+```
--- label.js
+++ label.js
@@ -1,37 +1,26 @@
 (function () {
     d3.labeler = function () {
-        var labeler = {},
-            w, h, lab = [], anc = [];
+        var labeler = {}, w, h, lab = [], anc = [];
 
         var max_move = 5.0,
+            max_angle = 0.5,
             acc = 0,
-            rej = 0,
-            max_angle = 0.5;
+            rej = 0;
 
         //weight
-        var weight_label2 = 30.0,
+        var weight_label = 30.0,
             weight_label_anc = 30.0,
-            w_orient = 3.0,
-            w_len = 0.2;
+            weight_len = 0.2;
 
         energy = function (index) {
             var m = lab.length,
                 ener = 0,
                 dx = lab[index].x - anc[index].x, //x dist between point and label
                 dy = anc[index].y - lab[index].y, //y dist between point and label
-                dist = Math.sqrt(dx * dx + dy * dy),
-                overlap = true;
+                dist = Math.sqrt(dx * dx + dy * dy);
 
             // penalty for length of leader line
-            if (dist > 0) ener += dist * w_len;
-
-            // label orientation bias
-            dx /= dist;
-            dy /= dist;
-            if (dx > 0 && dy > 0) { ener += 0 * w_orient; }
-            else if (dx < 0 && dy > 0) { ener += 1 * w_orient; }
-            else if (dx < 0 && dy < 0) { ener += 2 * w_orient; }
-            else { ener += 3 * w_orient; }
+            if (dist > 0) ener += dist * weight_len;
 
             var x21 = lab[index].x,
                 y21 = lab[index].y - lab[index].height + 2.0,
@@ -41,14 +30,15 @@
             for (var i = 0; i < m; i++) {
                 if (i != index) {
                     //label-label overlap
+                    //positions of 4 corners of rect bounding the text
                     x11 = lab[i].x,
-                        y11 = lab[i].y - lab[i].height + 2.0,
-                        x12 = lab[i].x + lab[i].width,
-                        y12 = lab[i].y + 2.0;
+                    y11 = lab[i].y - lab[i].height + 2.0,
+                    x12 = lab[i].x + lab[i].width,
+                    y12 = lab[i].y + 2.0;
                     x_overlap = Math.max(0, Math.min(x12, x22) - Math.max(x11, x21));
                     y_overlap = Math.max(0, Math.min(y12, y22) - Math.max(y11, y21));
                     overlap_area = x_overlap * y_overlap;
-                    ener += (overlap_area * weight_label2);
+                    ener += (overlap_area * weight_label);
                 }
                 //label point overlap
                 x11 = anc[i].x - anc[i].r; //x start point
@@ -66,14 +56,14 @@
         mcmove = function (currTemp) {
             var i = Math.floor(Math.random() * lab.length);
 
-            //save old location
+            //save old location of label
             var x_old = lab[i].x;
             var y_old = lab[i].y;
 
             //old energy
             var old_energy = energy(i);
 
-            //translate randomly
+            //move to a new position
             lab[i].x += (Math.random() - 0.5) * max_move;
             lab[i].y += (Math.random() - 0.5) * max_move;
 
@@ -88,58 +78,16 @@
             var delta_energy = new_energy - old_energy;
 
             if (Math.random() < Math.exp(-delta_energy / currTemp)) {
-                acc += 1;
+                // acc += 1;
+                // do nothing, label already at new pos
             } else {
-                //reject new position
+                //go back to the old pos
                 lab[i].x = x_old;
                 lab[i].y = y_old;
                 rej += 1;
             }
         }
 
-        mcrotate = function (currTemp) {
-            var i = Math.floor(Math.random() * lab.length);
-
-            //save old location
-            var x_old = lab[i].x;
-            var y_old = lab[i].y;
-
-            //old energy
-            var old_energy = energy(i);
-
-            var angle = (Math.random() - 0.5) * max_angle;
-            var s = Math.sin(angle);
-            var c = Math.cos(angle);
-
-            lab[i].x -= anc[i].x;
-            lab[i].y -= anc[i].y;
-            
-            //rotate
-            var newX = lab[i].x * c - lab[i].y * s;
-            var newY = lab[i].x * s + lab[i].y * c;
-            
-            lab[i].x = newX + anc[i].x;
-            lab[i].y = newY + anc[i].y;
-
-            if (lab[i].x > w) { lab[i].x = x_old; }
-            if (lab[i].x < 0) { lab[i].x = x_old; }
-            if (lab[i].y > h) { lab[i].y = y_old; }
-            if (lab[i].y < 0) { lab[i].y = y_old; }
-
-            //new energy
-            var new_energy = energy(i);
-            //change in energy
-            var delta_energy = new_energy - old_energy;
-
-            if (Math.random() < Math.exp(-delta_energy / currTemp)) {
-                acc += 1;
-            } else {
-                //reject new position
-                lab[i].x = x_old;
-                lab[i].y = y_old;
-                rej += 1;
-            }
-        }
         coolingTemp = function (currTemp, initialTemp, nsweeps) {
             return (currTemp - (initialTemp / nsweeps));
         }
@@ -150,11 +98,7 @@
                 initialTemp = 1.0;
             for (var i = 0; i < nsweeps; i++) {
                 for (var j = 0; j < m; j++) {
-                    if (Math.random() < 0.5) {
-                        mcmove(currTemp);
-                    } else {
-                        mcrotate(currTemp);
-                    }
+                    mcmove(currTemp);
                 }
                 currTemp = coolingTemp(currTemp, initialTemp, nsweeps);
             }
